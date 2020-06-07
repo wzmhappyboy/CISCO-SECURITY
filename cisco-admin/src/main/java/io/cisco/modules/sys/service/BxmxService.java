6bd369ad281dbc4703188b692dcdad23c52088c1
zimingw <zimingw@cisco.com>
2020-06-07 20:36:27 +0800

    6.7II

---
+++ cisco-admin/src/main/java/io/cisco/modules/sys/service/BxmxService.java
@@ -0,0 +1,9 @@
+package io.cisco.modules.sys.service;
+
+import io.cisco.modules.sys.entity.Bxmx;
+
+import java.util.List;
+
+public interface BxmxService {
+    List<Bxmx> queryPassBxmx(Long id);
+}
